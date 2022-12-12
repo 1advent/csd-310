92d451b2bcc330b0e6e60d735e3381df885895de
Joshua Welch <oneadvent@joshuawelch.me>
2022-12-11 20:25:53 -0800

    updated with all queries

--- venv/finance_report.py
+++ venv/finance_report.py
@@ -15,10 +15,28 @@ try:
     cursor = db.cursor()
     print("Average Asset Value")
     cursor.execute("select sum(transaction_amount) / count(distinct customer_id) from transaction_table;")
-    rows=cursor.fetchall()
+    rows = cursor.fetchall()
     for row in rows:
         for col in row:
-            print(col,end=" ")
+            print(round(col, 2), end=" ")
+        print()
+
+    cursor = db.cursor()
+    print("Clients with more than 10 transactions a month")
+    cursor.execute("select customer_table.name, count(transaction_table.transaction_amount) from transaction_table inner join customer_table on customer_table.customer_id=transaction_table.customer_id group by customer_table.name having count(transaction_table.transaction_amount) >= 10;")
+    rows = cursor.fetchall()
+    for row in rows:
+        for col in row:
+            print(col, end=" ")
+        print()
+
+    cursor = db.cursor()
+    print("New Clients added for each month in the past six months")
+    cursor.execute("WITH CTE AS (SELECT transaction_table.customer_id, MIN(transaction_table.DATETIME) AS datetimecol FROM transaction_table GROUP BY customer_id) SELECT date_format(datetimecol, '%Y-%m') as mnth, count(distinct customer_id) as num_joined FROM CTE GROUP BY date_format(datetimecol, '%Y-%m') order by mnth desc limit 6;")
+    rows = cursor.fetchall()
+    for row in rows:
+        for col in row:
+            print(col, end=" ")
         print()
 
     # Close connection
